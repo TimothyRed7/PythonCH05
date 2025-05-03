3d6a31f9c24697795521173f6f49b3f3f3a1d3ac
TimothyRed7 <tdavisreddish@students.avc.edu>
2025-05-03 05:58:41 +0000

    3

--- primemodule.py
+++ primemodule.py
@@ -12,5 +12,28 @@ def is_prime(n):
         return False
     if(t==0):
         return True
-print(is_prime(10))
-print(is_prime(11))
+def get_primes(n):
+    primes=[]
+    if(n>1):
+        for v in range(2,n-1):
+            y=v
+            x=0
+            z=0
+            while (z==0):
+                y=y-1
+                if(y>1):
+                    if(x==0 and v%y!=0):
+                        x=0
+                    else:
+                        x=1
+                        z=1
+                else:
+                    z=1
+            if(x==0):
+                primes.append(v)
+    return primes
+                
+def print_primes(n):
+    primes=get_primes(n)
+    for v in primes:
+        print(v, end=' ')
